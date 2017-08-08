fcad7b81b718e76c4be621c0a3673e30064d92bf
Ben Johnson <benjoh@amazon.com>
2017-08-07 23:46:33 -0700

    Implemented basic data collection and upload to S3

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # aws-lambda-pco-data-extractor
 A NodeJS AWS Lambda function to get PlanningCenterOnline data
+
+This app makes use of the node-lambda package:
+  https://www.npmjs.com/package/node-lambda
+
+To test running the function locally, run this:
+  ./node_modules/node-lambda/bin/node-lambda run --configFile deploy.env
+
+Where the deploy.env file contains the unencrypted credentials as
+environment variables, and a line with 'decrypted=true'.
+
+To update credentials for PCO, login to the AWS Console, and
+update the Lambda function configuration:
+  Create a Lambda Function Using Environment Variables To Store
+  Sensitive Information
+  http://docs.aws.amazon.com/lambda/latest/dg/tutorial-env_console.html
+
+PCO API Credentials can be managed here:
+  https://api.planningcenteronline.com/oauth/applications/
+
+PCO API Documentation can be found here:
+  https://planningcenter.github.io/api-docs
+
--- index.js
+++ index.js
@@ -1,72 +1,120 @@
 'use strict';
 
-let s3crypt = require('node-s3-encryption-client');
+const AWS = require('aws-sdk');
+AWS.config.region = 'us-west-2';
+let s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const https = require('https');
+const deasync = require('deasync');
+
+const decrypted = process.env['decrypted'];
+const rawAppId = process.env['app_id'];
+const rawSecret = process.env['secret'];
+let decryptedAppId;
+let decryptedSecret;
+
+function processEvent(event, context, callback) {
+  if (decryptedAppId !== undefined && decryptedSecret !== undefined){
+    let pcoData = getPCOData();
+    console.log("pcoData = " + JSON.stringify(pcoData));
+    uploadDataToS3(pcoData);
+  }
+}
 
 exports.handler = (event, context, callback) => {
-  console.log('event: ' + JSON.stringify(event));
-  if (event.putCreds) {
-    putCreds(event.putCreds);
+  if (decrypted) {
+    decryptedAppId = rawAppId;
+    decryptedSecret = rawSecret;
+    processEvent(event, context, callback);
   } else {
-    getCreds('pco', function(err, data){
-      if (err) console.log(err, err.stack);
-      else console.log(data);
-    });
+    decryptData(rawAppId, decryptedAppId, event, context, callback);
+    decryptData(rawSecret, decryptedSecret, event, context, callback);
   }
-}
-
-function putCreds(creds){
-  if (!creds.name) {
-    
-  }
-  var kmsParams = {
-    KeyId: 'arn:aws:kms:us-east-1:008838943100:alias/planningcenteronline',
-    KeySpec: 'AES_256'
-  };
-  var putParams = {
-    Bucket: 'www.downtowncornerstone.org',
-    Key: 'creds/' + creds.name + '.accessKey',
-    Body: creds.accessKey,
-    KmsParams: kmsParams
-  };
-  console.log(putParams);
-  s3crypt.putObject(putParams, function(err, data) {
-    if (err) console.log(err, err.stack);
-    else {
-      console.log(data);
-      putParams.Key = 'creds/' + creds.name + '.secretKey';
-      putParams.Body = creds.secretKey
-      putParams.KmsParams = kmsParams;
-      delete putParams.Metadata;
-      console.log(putParams);
-      s3crypt.putObject(putParams, function(err, data) {
-        if (err) console.log(err, err.stack);
-        else {
-          console.log(data);
-          console.log('Credentials Saved!');
-        }
-      });
-    }
-  });
-}
-
-function getCreds(name, callback){
-  var getParams = {
-    Bucket: 'www.downtowncornerstone.org',
-    Key: 'creds/' + name + '.accessKey',
-  };
-  s3crypt.getObject(getParams, function(err, data1) {
-    if (err) callback(err, null);
-    else {
-      // console.trace(data1);
-      getParams.Key = 'creds/' + name + '.secretKey',
-      s3crypt.getObject(getParams, function(err, data2) {
-        if (err) callback(err, null);
-        else {
-          // console.trace(data2);
-          callback(null, {accessKey: data1.Body, secretKey: data2.Body});
-        }
-      });
-    }
-  });
 };
+
+function decryptData(encrypted, decrypted, event, context, callback){
+  const kms = new AWS.KMS();
+  kms.decrypt({ CiphertextBlob: new Buffer(encrypted, 'base64') }, (err, data) => {
+    if (err) {
+      console.log('Decrypt error:', err);
+      return callback(err);
+    }
+    decrypted = data.Plaintext.toString('ascii');
+    processEvent(event, context, callback);
+  });
+}
+
+function getPCOData(){
+  var teamByDate = {};
+  var service_types = callPCO('/services/v2/service_types');
+  var gatheringId = service_types.data.filter( function(type) {
+    return type.attributes.name == "Gatherings"; })[0].id;
+  console.log("gaterhingId = " + gatheringId);
+
+  var plans = callPCO('/services/v2/service_types/' + gatheringId + '/plans?per_page=2');
+  var nextPage = plans.links.next;
+  console.log("nextPage = " + nextPage);
+  for (var plan of plans.data) {
+    var planDate = new Date(plan.attributes.dates).toISOString().substr(0,10);
+    console.log("planDate = " + planDate);
+    console.log("planId = " + plan.id);
+    var teammates = callPCO('/services/v2/service_types/' + gatheringId + '/plans/' + plan.id + '/team_members');
+    var team = [];
+    for ( var person of teammates.data ) {
+      var name = person.attributes.name.replace(/[a-z] ?/g,'');
+      var position = person.attributes.team_position_name.replace(/_/,'');;
+      //console.log("name = " + name);
+      //console.log("position = " + position);
+      team.push({name: name, position: position});
+    }
+    console.log("team = " + JSON.stringify(team));
+    teamByDate[planDate] = team;
+  }
+  return teamByDate;
+}
+
+function callPCO(path){
+  console.log("calling PCO for resource: " + path);
+  var options = {
+     host: 'api.planningcenteronline.com',
+     port: 443,
+     path: path,
+     headers: {
+       'Authorization': 'Basic ' + new Buffer(decryptedAppId + ':' + decryptedSecret).toString('base64')
+     }
+  };
+  var response;
+  var request = https.get(options, function(res){
+    var body = "";
+    res.on('data', function(data) {
+      body += data;
+    });
+    res.on('end', function() {
+      // console.log(body);
+      response = JSON.parse(body);
+    })
+    res.on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+  });
+  while(response === undefined) {
+    deasync.runLoopOnce();
+  }
+  return response;
+};
+
+function uploadDataToS3(obj){
+  var json = JSON.stringify(obj);
+  var uploadParams = {
+    Bucket: 'media.downtowncornerstone.org',
+    Key: 'teamDataByDate.json',
+    Body: json,
+    ContentType: 'application/json'};
+  s3.upload(uploadParams, function(err, data) {
+    if (err) {
+      console.log("Error uploading data: ", err);
+    } else {
+      console.log("Successfully updated data at " + uploadParams.Bucket + '/' + uploadParams.Key);
+    }
+  });
+}
 
